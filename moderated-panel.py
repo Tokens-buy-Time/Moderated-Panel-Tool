8e557605b53f39af9f5d38ba0ac0c6ea39b72d64
AI driven mobile App experimental demo team <Tokens.buy.Time@gmail.com>
2024-08-16 15:33:12 -0400

    Update moderated-panel.py
    Test

--- moderated-panel.py
+++ moderated-panel.py
@@ -46,12 +46,13 @@ for i in range(len(panelist_name)):
     if i > 7:
         break
 """
+
 # Add the discussion instructions 
 
-# prompt += "\n\n(c) The Moderator will use the user-provided topic to develop a series of 20 questions."
-# prompt += " \n\n(d) The questions will be presented to the panel members after the user switches to voice mode."
-# prompt += "\n(d) Each question will then be put to an individual panel member, one member at a time, while in voice mode."
-# prompt += "\n\n(c) The moderator is to put their 20 questions to the panel such that for each question the moderator will choose a particular panel member to kick off the conversation as the moderator determine most appropriate."
+# prompt += "\n\n(a03) The Moderator will use the user-provided topic to develop a series of 20 questions."
+# prompt += " \n\n(a04) The questions will be presented to the panel members after the user switches to voice mode."
+# prompt += "\n(a04) Each question will then be put to an individual panel member, one member at a time, while in voice mode."
+# prompt += "\n\n(a03) The moderator is to put their 20 questions to the panel such that for each question the moderator will choose a particular panel member to kick off the conversation as the moderator determine most appropriate."
 # prompt += "\n\n The user is to be requested to switch to verbal conversational AI mode, so that the conversation is audible on the device on which this session is executing."
 
 
@@ -69,12 +70,12 @@ st.text_area("Engineered Prompt:", prompt)
 
     Conversation shall continue with a view to :-
 
-    (a) have the panel discuss, using Conversational AI, where the participants are as previously defined,
+    (a01) have the panel discuss, using Conversational AI, where the participants are as previously defined,
 
-    (b) discus any given topic, where the topic is to be developed - from a brief, supplied by the Device User, as the initial input and is to be used to supplement the complete engineered prompt - which defines the overall assignment - as raw training data contained within the uploaded document and committed to memory.
+    (a02) discus any given topic, where the topic is to be developed - from a brief, supplied by the Device User, as the initial input and is to be used to supplement the complete engineered prompt - which defines the overall assignment - as raw training data contained within the uploaded document and committed to memory.
 
-    (c) the Moderator using the user provided briefing, PDF file uploaded, to develop the aforementioned series of 20 questions, to be presented to the panel members after the user switches to voice mode,
-    (d) each question then to be put to an individual panel member; one member at a time, while in voice mode.
+    (a03) the Moderator using the user provided briefing, PDF file uploaded, to develop the aforementioned series of 20 questions, to be presented to the panel members after the user switches to voice mode,
+    (a04) each question then to be put to an individual panel member; one member at a time, while in voice mode.
 
     (B.1) The different voices are as follows :-
 
