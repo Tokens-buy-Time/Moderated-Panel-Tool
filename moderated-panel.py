a96d066943f5171f7b16dee55f0b86699afc52fd
AI driven mobile App experimental demo team <Tokens.buy.Time@gmail.com>
2024-08-16 03:01:00 -0400

    Update moderated-panel.py
    Tedt

--- moderated-panel.py
+++ moderated-panel.py
@@ -7,12 +7,33 @@ st.title("Moderated Panel Tool")
 
 
 # Section for Panelists
+def panel("panrlist_name", "panelist_expertise", panelist_emulated_individual")
+
 st.header("Panelists")
+
 panelist_name = []
 panelist_expertise = []
 panelist_emulated_individual = []
 
 for i in range(1, 8):  # Assuming 7 panelists maximum
+
+    st.session_state["panelist_name"][i] = input
+    st.session_state["panelist_expertise"][i] = input
+    st.session_state["panelist_emulated_individual"][i] = input
+
+    
+
+    # Display Panelists
+def panel_start():
+    
+    if st.button("List panelist"):
+        
+        st.subheader(" This session's panelist will be :-")
+        
+        for i, panel in enumerate(panelist_name):
+            st.write(f"Panelist No. {i+1}: {panelist_name}")
+    
+    
     panelist_name.append(st.text_input(f"Panelist No. {i} name :"))
     panelist_expertise.append(st.text_input(f"Panelist No. {i} area of expertise :"))
     panelist_emulated_individual.append(st.text_input(f"Panelist No. {i} similar to :"))
@@ -23,10 +44,5 @@ for i in range(1, 8):  # Assuming 7 panelists maximum
     
     write(" ")
 
-# Display Panelists
-def panel_start():
-    if st.button("List panelist"):
-        st.subheader(" This session's panelist will be :-")
-        for i, panel in enumerate(panelist_name):
-            st.write(f"Panelist No. {i+1}: {panelist_name}")
+
 
