0822bc87ba93a3ab2d227d1741dde6874bef9670
AI driven mobile App experimental demo team <Tokens.buy.Time@gmail.com>
2024-08-16 02:32:58 -0400

    Update moderated-panel.py
    Test

--- moderated-panel.py
+++ moderated-panel.py
@@ -6,9 +6,6 @@ import numpy
 # Title of the app
 st.title("Moderated Panel Tool")
 
-# Section for Moderator
-st.header("Moderator")
-moderators_name = st.text_input("Enter name of moderator")
 
 # Section for Panelists
 st.header("Panelists")
@@ -21,18 +18,10 @@ for i in range(1, 7):  # Assuming 7 panelists maximum
     panelist_expertise.append(st.text_input(f"Panelist No. {i} area of expertise :"))
     panelist_emulated_individual.append(st.text_input(f"Panelist No. {i} similar to :"))
 
-# Display Responses
+# Display Panelists
 def panel_start():
-    if st.button(""):
+    if st.button("List panelist"):
         st.subheader(" This session's panelist will be :-")
         for i, panel in enumerate(panelist_name):
             st.write(f"Panelist No. {i+1}: {panelist_name}")
 
-# Section for the moderator
-def set_moderator(moderator_name):
-    st.header("Moderator")
-    moderator_name = st.text_input("This session's moderator will be : {moderator_name}")
-
-if st.button("Waiting on moderator"):
-    set_moderator(moderator_name):
-st.write(f"This session, moderated by {moderator_name} will now begin")
